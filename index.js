d8b9a68298bcab18c840c8c81dc0922404310e33
Nick Niemeir <nick.niemeir@gmail.com>
2017-02-12 18:41:26 -0700

    Setup the binary.

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+#!/usr/bin/env node
+
 var cp = require('child_process')
 
 cp.exec("git diff --staged --diff-filter=ACMTUXB --name-only -- '*.js'", { encoding: 'utf8' }, function (err, stdout, stderr) {
+  if (stderr) console.error(stderr)
   if (err) {
     throw err
   }
-  console.error(stderr)
+
   stdout.split('\n')
     .reduce(function (next, name, idx) {
-      return function (code) {
-        if (name) {
-          var contents = cp.spawn('git', [ 'show', ':' + name ])
-          var lint = cp.spawn('eslint', [ '--color', '--stdin', '--stdin-filename=' + name ])
-          contents.stdout.pipe(lint.stdin)
-          contents.stderr.pipe(process.stderr)
+      if (name) {
+        var contents = cp.spawn('git', [ 'show', ':' + name ])
+        var lint = cp.spawn('eslint', [ '--color', '--stdin', '--stdin-filename=' + name ])
+        contents.stdout.pipe(lint.stdin)
+        contents.stderr.pipe(process.stderr)
+        return function (code) {
           lint.stdout.pipe(process.stdout)
           lint.stderr.pipe(process.stderr)
           lint.on('close', function (exitCode) {
             next(exitCode > code ? exitCode : code)
           })
-        } else {
+        }
+      } else {
+        return function (code) {
           process.nextTick(function () {
             next(code)
           })
